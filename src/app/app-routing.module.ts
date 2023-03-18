185a6765855a89cfcb36eb0115018adc6b27fd27
eloge257 <niremaeloge@gmail.com>
2023-03-18 16:01:52 +0100

    call  login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FormulaireShopComponent } from "./formulaire-shop/formulaire-shop.component";
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
   {path : 'home-component', component : HomeComponent},
-  {path : 'formulaireShop-component', component : FormulaireShopComponent}
+  {path : 'formulaireShop-component', component : FormulaireShopComponent},
+  {path : 'login-component', component: LoginPageComponent }
 ];
 
 @NgModule({
