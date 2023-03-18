7e93eeed26e1e865128d5a24f7791b1d2cf7756b
eloge257 <niremaeloge@gmail.com>
2023-03-18 15:22:25 +0100

    add a compenent login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormulaireShopComponent } from './formulaire-shop/formulaire-shop.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FormulaireShopComponent
+    FormulaireShopComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
